b24e1eb047c616c279f88e8042d6fb77fab12c63
Densovan <sarimramata@gmail.com>
2022-10-27 17:07:03 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import dynamic from "next/dynamic";
+// const PlayerWithNoSSR = dynamic(() => import("../components/Player"), {
+//   ssr: false,
+// });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
