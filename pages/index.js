bc6b3bab6416c3969be3c50c42de9b28152d580b
Densovan <sarimramata@gmail.com>
2022-11-14 22:14:32 +0700

    fixed pdf offline

--- pages/index.js
+++ pages/index.js
@@ -10,34 +10,41 @@ export default function Home() {
       .then((res) => res.json())
       .then((data) => {
         setItem(data);
+        setLoading(false);
       });
   }, []);
 
   return (
     <>
-      <br />
-      {/* <------ category section -------> */}
-      <div className="p-2 md:p-12">
-        <br />
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 md:gap-4 gap-2">
-          {item.map((items, i) => (
-            <Card
-              key={i}
-              gradeName={items.category_display_name}
-              title={items.display_name}
-              thumbnail={items.thumbnail}
-              grade={items.grade}
-              subject={items.subject}
-              fileType={items.file_type}
-              location={items.location}
-              gradeKh={items.grade_kh}
-              subjectKh={items.subject_kh}
-              thumbnailName={items.thumbnail.thumbnail_name}
-              id={items.file_id}
-            />
-          ))}
-        </div>
-      </div>
+      {loading ? (
+        <center>Loading...</center>
+      ) : (
+        <>
+          <br />
+          {/* <------ category section -------> */}
+          <div className="p-2 md:p-12">
+            <br />
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 md:gap-4 gap-2">
+              {item.map((items, i) => (
+                <Card
+                  key={i}
+                  gradeName={items.category_display_name}
+                  title={items.display_name}
+                  thumbnail={items.thumbnail}
+                  grade={items.grade}
+                  subject={items.subject}
+                  fileType={items.file_type}
+                  location={items.location}
+                  gradeKh={items.grade_kh}
+                  subjectKh={items.subject_kh}
+                  thumbnailName={items.thumbnail.thumbnail_name}
+                  id={items.file_id}
+                />
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
